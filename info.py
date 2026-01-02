8aaecbfbcd512771e63b8939d5f894ca31aa9768
RAM007 <151729806+ram0069@users.noreply.github.com>
2026-01-02 16:20:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ class Config(object):
     # Other Configs
     BOT_START_TIME = time.time()
     OWNER    = int(os.environ.get("OWNER", "7366293185"))  # ⚠️ Required
-    SUDO = list(map(int, os.environ.get("SUDO", "7366293185", "5877373835").split()))  # ⚠️ Required
+    SUDO = list(map(int, os.environ.get("SUDO", "7366293185 5877373835").split()))
     # Web Response Config
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8080"))
