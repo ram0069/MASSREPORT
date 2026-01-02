2f7a6a6b44c8807b38ddb3e3776e4af757777b91
RAM007 <151729806+ram0069@users.noreply.github.com>
2026-01-02 16:03:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ async def handle_start(bot:Client, message:Message):
 
     Btn = [
         [InlineKeyboardButton(text='⛑️𝖧𝖤𝖫𝖯🚁', callback_data='help'), InlineKeyboardButton(text='🌀𝖡𝖮𝖳 𝖲𝖳𝖠𝖳𝖴𝖲✳️', callback_data='server')],
-        [InlineKeyboardButton(text='📰𝖴𝖯𝖣𝖠𝖳𝖤🗞️', url='https://t.me/alltypecc'), InlineKeyboardButton(text='🤖𝖡𝖮𝖳 𝖨𝖭𝖥𝖮ℹ️', callback_data='about')],
+        [InlineKeyboardButton(text='📰𝖴𝖯𝖣𝖠𝖳𝖤🗞️', url='https://t.me/qrynn'), InlineKeyboardButton(text='🤖𝖡𝖮𝖳 𝖨𝖭𝖥𝖮ℹ️', callback_data='about')],
         [InlineKeyboardButton(text='🧑‍💻𝖣𝖤𝖵⌨️', url='https://t.me/qrynn')]
         ]
 
